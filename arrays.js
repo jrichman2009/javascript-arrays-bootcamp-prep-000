9590b16583221c647f96c3e640085211310b9b3c
Jeffrey Richman <jrichman2009@gmail.com>
2018-08-19 15:31:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,4 +5,6 @@ function addElementToBeginningOfArray(array, element) {
   return new_array
 }
 
-function()
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+}
